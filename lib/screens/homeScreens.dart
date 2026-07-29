b69c74b958ce2b2655c4b94d72ad521941d62f9a
nashirabbash <nashirabbash@gmail.com>
2026-07-29 11:28:15 +0700

    fix(home): add async mounted check after await nav.push(ScanScreen)

--- lib/screens/homeScreens.dart
+++ lib/screens/homeScreens.dart
@@ -132,6 +132,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               builder: (context) => const ScanScreen(),
                             ),
                           );
+                          if (!mounted) return;
                           _checkDeviceBinding();
                         },
                       ),
